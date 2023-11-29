0cb5fc81a1db87763048abe119195d77b757768e
Souvik Das <souvik968422@gmail.com>
2023-11-29 16:58:47 +0530

    About & Contact added

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import Header from './Header'
-
+import React from 'react';
+import Header from './Header';
+//import './About.css'; // Assuming you have a separate CSS file for styling
 
 export default function About() {
   return (
     <div>
       <Header />
-      <header>
-        <h1>About</h1>
+      <header className="about-container">
+        <h1 style={{color:'aqua'}}>About My Booksite Website</h1>
 
-        
-        
+        <p>
+          Welcome to My Booksite, a platform dedicated to the world of literature and the joy of reading.
+        </p>
+
+        <p>
+          Our mission is to provide a curated collection of books that cater to diverse tastes and interests. Whether you are a seasoned reader or just starting your reading journey, My Booksite offers a wide range of genres and authors to explore.
+        </p>
+
+        <p>
+          Discover new stories, immerse yourself in captivating narratives, and connect with fellow book enthusiasts. My Booksite is more than just a website; it's a community that celebrates the magic of books and the shared love for storytelling.
+        </p>
       </header>
     </div>
-  )
+  );
 }
--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import Header from './Header'
+// Contact.js
+
+import React from 'react';
+import Header from './Header';
+//import './Contact.css'; // Assuming you have a separate CSS file for styling
 
 export default function Contact() {
   return (
     <div>
       <Header />
-      <div className='contact-links'>
+      <div className='contact-container'>
         <p>
-          Follows us on LinkedIn
+          Follow us on 
+          <span className='contact-link'>
+            <a href="https://www.linkedin.com/in/souvik-das-b8115916b/" target="_blank" rel="noopener noreferrer">
+              LinkedIn <i className="fab fa-linkedin"></i>
+            </a>
+          </span>
         </p>
         <p>
-          Follows us on Instagram
+          Follow us on 
+          <span className='contact-link'>
+            <a href="https://www.instagram.com/old_star11/" target="_blank" rel="noopener noreferrer">
+              Instagram <i className="fab fa-instagram"></i>
+            </a>
+          </span>
         </p>
+        
       </div>
     </div>
-  )
+  );
 }
